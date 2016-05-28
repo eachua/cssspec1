b92b113298619712a12527efabf5223bc17828b7
Eldric Earl A. Chua <ldrc_26@yahoo.com>
2016-05-28 13:34:36 +0800

    Update accept.cbl

--- accept.cbl
+++ accept.cbl
@@ -1,8 +1,18 @@
-identification division.
-program-id.accept.
-environment division.
-data division.
-working-storage section.
-77 a pic s9(3)v9(2) value 0.
-77 b pic s9(3)v9(2) value 0.
-77
+ IDENTIFICATION DIVISION.
+ PROGRAM-ID.ACCPT.
+ ENVIRONMENT DIVISION.
+ DATA DIVISION.
+ WORKING-STORAGE SECTION.
+ 77 A pic s9(3)v9(2) value 0.
+ 77 B pic s9(3)v9(2) value 0.
+ 77 F-G pic +z(3).z(2).
+
+ PROCEDURE DIVISION.
+ para-1.
+        DISPLAY "ENTER FIRST NUMBER :"WITH NO ADVANCING.
+        DISPLAY "ENTER SECOND NUMBER :"WITH NO ADVANCING.
+        ACCEPT B.
+        SUBTRACT A FROM B.
+        MOVE B TO F-G.
+        DISPLAY "B-A = " F-G.
+ STOP RUN.
